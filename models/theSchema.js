aa77178c5e4187a3b15b06cec4e1e1f2d123f637
eduardopaaaa <eddy.nator@hotmail.com>
2022-09-26 12:53:34 -0600

    heroku test

--- models/theSchema.js
+++ models/theSchema.js
@@ -1,12 +1,9 @@
 const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
-    name: String,
-    type: String,
-    task: String,
-    date: String,
-    importance: String,
-    completed: Boolean
+    Assigned: String,
+    Priority: String,
+    Description: String,
 
 })
 
